a74974eeeb02093d61f37edb9fd1bd1ce7a7bbaf
tuntun <tuntun@juanpi.com>
2015-07-14 09:28:50 +0800

    Python3下载MongoDB上的文件

--- testmongo.py
+++ testmongo.py
@@ -1,13 +1,24 @@
-
 from pymongo import MongoClient
-import datetime
+import gridfs
+import time
+import re
+import os
+import sys
 
-client = MongoClient('mongodb://tanteng:123456@localhost:27017/')
+client = MongoClient('mongodb://192.168.1.22:27017/excel')
 db = client.js_send_excel
+fs = gridfs.GridFS(db)
+files = fs.find()
 
-post = {"author": "Xiaofeng", "text": "My first blog post!",
-		"tags": ["mongodb", "python", "pymongo"],}
+time_start = time.clock()
+print('总数：', files.count())
 
-posts = db.posts
-post_id = posts.insert_one(post).inserted_id
-print(post_id)
+for ffle in files:
+    filename = ffle.filename
+    m = re.match(r'发货订单', filename)
+
+    if m and filename.find('.xls') > 0 and not os.path.isfile('./excel/' + filename):
+        with open('./excel/' + filename, 'wb') as f1:
+            f1.write(ffle.read())
+
+print('执行时间{0}秒'.format(time.clock() - time_start))
