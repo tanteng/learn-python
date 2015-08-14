22dcadaafbc8121308305a000552c481cf92b47a
tuntun <tuntun@juanpi.com>
2015-08-14 18:20:20 +0800

    使用python操作MongoDB的GridFS下载文件

--- testmongo.py
+++ testmongo.py
@@ -1,24 +1,31 @@
+# -*- coding:utf-8 -*-
+
 from pymongo import MongoClient
-import gridfs
 import time
 import re
 import os
+import gridfs
 import sys
 
-client = MongoClient('mongodb://192.168.1.22:27017/excel')
+client = MongoClient('mongodb://m_user:m_user_027@192.168.5.22:27017/js_send_excel')
 db = client.js_send_excel
 fs = gridfs.GridFS(db)
+
 files = fs.find()
 
 time_start = time.clock()
 print('总数：', files.count())
+count = 0
 
 for ffle in files:
     filename = ffle.filename
+
     m = re.match(r'发货订单', filename)
 
-    if m and filename.find('.xls') > 0 and not os.path.isfile('./excel/' + filename):
-        with open('./excel/' + filename, 'wb') as f1:
+    if m and filename.find('xls') > 0 and not os.path.isfile('./excel/' + filename):
+        with open('./excel2/' + filename, 'wb') as f1:
             f1.write(ffle.read())
+            count += 1
 
 print('执行时间{0}秒'.format(time.clock() - time_start))
+print('本次更新{0}个文档'.format(count))
