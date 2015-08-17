55806ce121a05a9e72760567c833064eb0d42800
tuntun <tuntun@juanpi.com>
2015-08-17 18:00:37 +0800

    Python定义dict

---
+++ me.py
@@ -0,0 +1,12 @@
+# Python dict
+me = {
+    'name': 'tanteng',
+    'age': 25,
+    'sex': 'man',
+    'city': 'Wuhan',
+    'college': 'WUST',
+    'profession': 'Computer Science and Technology',
+    'language': ['PHP', 'JavaScript', 'MySQL', 'Redis', 'MongoDB', 'Python'],
+    'website': 'http://www.tantengvip.com',
+    'email': 'tanteng@gmail.com'
+}
