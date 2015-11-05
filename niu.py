ecb27ab0bbcab5fc5d844cfbba688556507566cb
tuntun <tuntun@juanpi.com>
2015-11-05 17:34:05 +0800

    列举一副牌所有牛牛的情况

--- niu.py
+++ niu.py
@@ -1,48 +1,54 @@
+# -*- coding:utf-8 -*-
 
 # 列举一副牌所有牌型为牛牛的情况
-# author:tanteng
 
-import itertools,sys
-#初始化一副牌（除去大小王）
+__author__ = 'tanteng'
+
+import itertools, sys
+
+
+# 初始化一副牌（除去大小王）
 def cards():
-	cards = {'黑桃A':1,'红桃A':1,'方块A':1,'梅花A':1}
-	card_type = ['黑桃','红桃','方块','梅花']
+    cards = {'黑桃A': 1, '红桃A': 1, '方块A': 1, '梅花A': 1}
+    card_type = ['黑桃', '红桃', '方块', '梅花']
 
-	for c_type in card_type:
-		for no in range(2,11):
-			cards[c_type+str(no)] = no
+    for c_type in card_type:
+        for no in range(2, 11):
+            cards[c_type + str(no)] = no
 
-		for name in ['J','Q','K']:
-			cards[c_type+name] = 10
+        for name in ['J', 'Q', 'K']:
+            cards[c_type + name] = 10
 
-	return cards
+    return cards
 
-#从一副牌取5张牌所有排列
+
+# 从一副牌取5张牌所有排列
 def all_cases(cards):
-	return list(itertools.combinations(cards,5))
+    return list(itertools.combinations(cards, 5))
+
 
 if __name__ == '__main__':
-	cards = cards()
-	all_cases = all_cases(cards)
+    cards = cards()
+    all_cases = all_cases(cards)
 
-	print(len(all_cases))
+    print(len(all_cases))
 
-	for case in all_cases:
-		temp_name = list(case)
-		temp_value = []
-		for ca in case:
-			temp_value.append(cards[ca])
+    for case in all_cases:
+        temp_name = list(case)
+        temp_value = []
+        for ca in case:
+            temp_value.append(cards[ca])
 
-		sums = sum(temp_value)
-		if sums%10==0:
-			s_cases = list(itertools.combinations(temp_name,3))
-			for s_case in s_cases:
-				temp_name2 = list(s_case)
-				temp_value2 = []
-				for s_ca in s_case:
-					temp_value2.append(cards[s_ca])
+        sums = sum(temp_value)
+        if sums % 10 == 0:
+            s_cases = list(itertools.combinations(temp_name, 3))
+            for s_case in s_cases:
+                temp_name2 = list(s_case)
+                temp_value2 = []
+                for s_ca in s_case:
+                    temp_value2.append(cards[s_ca])
 
-				sumc = sum(temp_value2)
-				if sumc%10==0:
-					print(temp_name)
-					break
+                sumc = sum(temp_value2)
+                if sumc % 10 == 0:
+                    print(temp_name)
+                    break
