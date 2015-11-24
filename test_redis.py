a4bca7c45b3eeccc777cd9e9995b73ea00756a47
tuntun <tuntun@juanpi.com>
2015-11-24 14:37:55 +0800

    python3下操作redis

--- test_redis.py
+++ test_redis.py
@@ -1,8 +1,27 @@
-import redis
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-r.set('foo', 'bar')
+# encoding:utf-8
 
+import redis
+
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+r.set('foo', 'bar')
 foo = r.get('foo')
 
-print(foo)
-# b'bar'
+r.sadd('collection','abc')
+r.sadd('collection','def')
+r.sadd('collection','fhi')
+r.sadd('collection','xwy')
+
+r.zadd('rank',1,'Alibaba')
+r.zadd('rank',2,'Tencent')
+r.zadd('rank',3,'Baidu')
+r.zadd('rank',baidu=3,cc=3,ccc=34)
+r.zadd('rank',4,6)
+
+r.rpush('list','www.tantengvip.com')
+r.rpush('list','www.qq.com')
+r.rpush('list','www.tencent.com')
+
+pop = r.lpop('list')
+print(pop)
