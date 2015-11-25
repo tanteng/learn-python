ae18bec3a51a67e34c881a927f9bc969c33a5666
tuntun <tuntun@juanpi.com>
2015-11-25 10:29:52 +0800

    Python3命令行

--- sys_argv.py
+++ sys_argv.py
@@ -1,24 +1,42 @@
+# encoding:utf-8
+
+# !/usr/bin/env python
+
+# -*- Python3命令行 -*-
+
+import optparse
 import sys
 
-# sys.argv接收参数，第一个参数是文件名，第二个参数开始是用户输入的参数，以空格隔开
 
-def run1():
-    print('I\'m action1')
+def process_command_line(argv):
+    """
+    Return a 2-tuple: (settings object, args list).
+    `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
+    """
+    if argv is None:
+        argv = sys.argv[1:]
+    # initialize the parser object:
+    parser = optparse.OptionParser(
+        formatter=optparse.TitledHelpFormatter(width=78),
+        add_help_option=None)
+    # define options here:
+    parser.add_option(      # customized description; put --help last
+        '-h', '--help', action='help',
+        help='Show this help message and exit.')
+    settings, args = parser.parse_args(argv)
+    # check number of arguments, verify values, etc.:
+    if args:
+        parser.error('program takes no command-line arguments; '
+                     '"%s" ignored.' % (args,))
+    # further process settings & args if necessary
+    return settings, args
 
+def main(argv=None):
+    settings, args = process_command_line(argv)
+    # application code here, like:
+    # run(settings, args)
+    return 0        # success
 
-def run2():
-    print('I\'m action2')
-
-
-if 2 > len(sys.argv):
-    print('none')
-else:
-    action1 = sys.argv[1]
-    action2 = sys.argv[2]
-
-    if 'run1' == action1:
-        run1()
-    if 'run2' == action2:
-        run2()
-        
-#用法：在命令行执行脚本，python sys_argv.py run1 run2
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
