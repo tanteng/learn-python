7ccdf0710aa092aef828eb03bb52ea68ba9b63e9
tuntun <tuntun@juanpi.com>
2015-12-01 15:04:02 +0800

    使用requests保存远程图片(或文件)

---
+++ save_remote_image.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# 使用requests保存远程图片(或文件)
+import requests
+import os
+
+
+def saveRemoteImage():
+    imgurl = 'http://www.tantengvip.com/wp-content/uploads/2015/01/cropped-13887362_13887362_1347773771848.jpg'
+    filename = imgurl.split('/')[-1]
+    path = './static/'+filename
+    if not os.path.exists(path):
+        r = requests.get(imgurl)
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            print('OK')
+    else:
+        print('Already exists.')
+
+    """
+    下载大文件这样写：
+    for chunk in r.iter_content():
+        f.write(chunk)
+    """
+
+saveRemoteImage()
