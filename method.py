1d863b9a0e0feda09df40c02025ba7d3d1c6bf39
tuntun <tuntun@juanpi.com>
2015-12-01 14:08:07 +0800

    Python面向对象：类，类的方法，类方法，静态方法

--- method.py
+++ method.py
@@ -1,18 +1,45 @@
 # -*- coding: utf-8 -*-
 
-# Python 的cls
+# Python面向对象：类，类的方法，类方法，静态方法
 
-class demo(object):
-    def foo(cls):
+class Person(object):
+    def __init__(self):
+        print('init')
+
+    @staticmethod
+    def sayHello(hi):
+        if hi is None:
+            hi = 'hello'
+        print(hi)
+
+    @classmethod
+    def hi(cls,msg):
+        print(msg)
         print(dir(cls))
 
-demo = demo()
-demo.foo()
+    # 一般类的方法
+    def hobby(self,hobby):
+        print(hobby)
+
+# 调用静态方法，不用实例化
+Person.sayHello('hi')
+Person.hi('Hi!')
+
+# 实例化类调用普通方法,__init__在这里触发
+person = Person()
+person.hobby('football')
+
 
 """
-demo.foo()输出的结果跟dir(demo)的结果一样：
+输出：
+hi
+Hi!
 ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof_
-_', '__str__', '__subclasshook__', '__weakref__', 'foo']
+_', '__str__', '__subclasshook__', '__weakref__', 'hi', 'hobby', 'sayHello']
+init
+football
+
+其中def hi(cls)这个类方法，cls表示类自身，所以输出跟dir(person)是一样的。
 
 classmethod：类方法
 staticmethod：静态方法
