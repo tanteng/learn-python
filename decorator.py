0d9395b14cfeefaab02fa0f9cb7f7201d9e9947d
tuntun <tuntun@juanpi.com>
2015-12-09 11:00:48 +0800

    Python装饰器

--- decorator.py
+++ decorator.py
@@ -18,4 +18,7 @@ now()
 D:\learn-python>python decorator.py
 hi,now is:
 2015-12-9
+
+关于装饰器的具体概念，参见：
+http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386819879946007bbf6ad052463ab18034f0254bf355000
 """
