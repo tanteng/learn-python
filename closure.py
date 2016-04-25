6347e16a49e565705145b2f059db1340a6f21b7a
lake.lai <yaopower@vip.qq.com>
2016-04-25 15:16:05 +0800

    python中的闭包 for python 3.x
    UnboundLocalError: local variable 'count' referenced before assignment.
    
    什么意思?就是说conut这个变量你没有定义就直接引用了,我不知道这是个什么东西,程序就崩溃了.于是,再python3里面,引入了一个关键字:nonlocal,这个关键字是干什么的?就是告诉python程序,我的这个count变量是再外部定义的,你去外面找吧.然后python就去外层函数找,然后就找到了count=0这个定义和赋值,程序就能正常执行了.

--- closure.py
+++ closure.py
@@ -1,14 +1,27 @@
 def hellocounter (name):
-    count=[0]
+    count=0 #PYTHON 2.x 中，要写count=[0]
     def counter():
-        count[0]+=1
-        print('Hello,',name,',',count[0],' access!')
+        nonlocal count #PYTHON 2.x 中，此行和下一行要换成count[0]+=1
+        count+=1
+        print('Hello,',name,',',count,' access!')#PYTHON 2.x 中请自觉换成str(count[0])
     return counter
 
 hello = hellocounter('ma6174')
 hello()
 hello()
 hello()
+
+'''
+执行结果
+>>> hello()
+Hello, ma6174 , 1  access!
+>>> hello()
+Hello, ma6174 , 2  access!
+>>> hello()
+Hello, ma6174 , 3  access!
+'''
+##为什么PYTHON 2.x中不直接写count而用list？这是python2的一个bug,如果用count话,会报这样一个错误:
+##UnboundLocalError: local variable 'count' referenced before assignment.
 
 def make_adder(addend):
     def adder(augend):
